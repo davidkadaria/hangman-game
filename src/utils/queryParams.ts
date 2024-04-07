132955d16e68453ccc09e274f81fe3628d89fad1
DavidKadaria <david.qada@gmail.com>
2024-04-07 22:47:43 +0400

    Created queryParams util module

---
+++ src/utils/queryParams.ts
@@ -0,0 +1,18 @@
+const getQueryParam = (key: string) => {
+	const queryParams = new URLSearchParams(window.location.search);
+	return queryParams.get(key);
+};
+
+const setQueryParams = (key: string, value: string) => {
+	const queryParams = new URLSearchParams(window.location.search);
+	queryParams.set(key, value);
+	window.history.pushState({}, '', `${window.location.pathname}?${queryParams}`);
+};
+
+const removeQueryParam = (key: string) => {
+	const queryParams = new URLSearchParams(window.location.search);
+	queryParams.delete(key);
+	window.history.pushState({}, '', `${window.location.pathname}?${queryParams}`);
+};
+
+export { getQueryParam, setQueryParams, removeQueryParam };
