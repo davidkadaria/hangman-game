d490c600a2fab17257fb1d6359cb99877507ebfe
DavidKadaria <david.qada@gmail.com>
2024-04-07 23:22:28 +0400

    Setup basic querystring-based routing in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { useState, useEffect } from 'react';
+import { Screen } from './components';
+import { getQueryParam, setQueryParam } from './utils';
+
 import './styles/theme.css';
 import './styles/main.css';
 
-
 function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
+	const [currentTab, setCurrentTab] = useState('home');
+
+	const handleHistoryChange = () => {
+		const tab = getQueryParam('tab') || 'home';
+		setCurrentTab(tab);
+		setQueryParam('tab', tab);
+	};
+
+	useEffect(() => {
+		// Run handleHistoryChange when the component mounts
+		handleHistoryChange();
+
+		// Add an event listener to listen for changes to the history
+		window.addEventListener('popstate', handleHistoryChange);
+
+		// Clean up
+		return () => {
+			window.removeEventListener('popstate', handleHistoryChange);
+		};
+	}, []);
+
+	return (
+		<div className='App'>
+			<Screen page={currentTab} />
+		</div>
+	);
 }
 
 export default App;
