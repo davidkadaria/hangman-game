4f1291e731f56466b6c42cc4ecd012578c5f60a7
DavidKadaria <david.qada@gmail.com>
2024-04-10 00:16:35 +0400

    Modify PopupCard component

--- src/components/PopupCard/index.tsx
+++ src/components/PopupCard/index.tsx
@@ -1,8 +1,17 @@
 import type { Props } from './types';
 import './PopupCard.css';
 
-function PopupCard(props: Props) {
-	return <></>;
+function PopupCard({ title, supTitle, subTitle, blurBackground, children }: Props) {
+	return (
+		<div className={`PopupCard${blurBackground ? ' PopupCard--blurred-background' : ''}`}>
+			<h1 className='PopupCard__heading'>
+				{supTitle && <span className='PopupCard__supheading'>{supTitle}</span>}
+				{title}
+				{subTitle && <span className='PopupCard__subheading'>{subTitle}</span>}
+			</h1>
+			<div className='PopupCard__content'>{children}</div>
+		</div>
+	);
 }
 
 export { PopupCard };
--- src/components/PopupCard/types.ts
+++ src/components/PopupCard/types.ts
@@ -1,3 +1,9 @@
-type Props = {};
+type Props = {
+	title: string;
+	supTitle?: string;
+	subTitle?: string;
+	blurBackground?: boolean;
+	children: React.ReactNode;
+};
 
 export type { Props };
