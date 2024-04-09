122644ec2a1e8f0fe5a315473eeee6fdcfd616fc
DavidKadaria <david.qada@gmail.com>
2024-04-09 16:51:50 +0400

    Modify queryParams: use replaceState instead of pushstate for URL stack updation

--- src/utils/queryParams.ts
+++ src/utils/queryParams.ts
@@ -6,13 +6,13 @@ const getQueryParam = (key: string) => {
 const setQueryParam = (key: string, value: string) => {
 	const queryParams = new URLSearchParams(window.location.search);
 	queryParams.set(key, value);
-	window.history.pushState({}, '', `${window.location.pathname}?${queryParams}`);
+	window.history.replaceState({}, '', `${window.location.pathname}?${queryParams}`);
 };
 
 const removeQueryParam = (key: string) => {
 	const queryParams = new URLSearchParams(window.location.search);
 	queryParams.delete(key);
-	window.history.pushState({}, '', `${window.location.pathname}?${queryParams}`);
+	window.history.replaceState({}, '', `${window.location.pathname}?${queryParams}`);
 };
 
 export { getQueryParam, setQueryParam, removeQueryParam };
