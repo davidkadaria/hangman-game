02e081f289396ebd4c089adc9f4290ef7213d490
DavidKadaria <david.qada@gmail.com>
2024-04-09 16:15:55 +0400

    Change page validator util function; Validate based on key name

--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -1,14 +1,9 @@
 import { pageHierarchy } from '../constants';
 
 function isPageValid(page: string): boolean {
-	// Get array of page IDs
-	const pageIds = Object.keys(pageHierarchy).map((key) => pageHierarchy[key].id);
+	const pageIds = Object.keys(pageHierarchy);
 
-	if (pageIds.includes(page)) {
-		return true;
-	}
-
-	return false;
+	return pageIds.includes(page);
 }
 
 export { isPageValid };
