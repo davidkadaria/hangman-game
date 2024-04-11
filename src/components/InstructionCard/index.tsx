211843e50f9ae73a2f8bde9c72fb8806351a8d29
DavidKadaria <david.qada@gmail.com>
2024-04-12 01:08:37 +0400

    Implement InstructionCard component and its styles (including media queries)

--- src/components/InstructionCard/index.tsx
+++ src/components/InstructionCard/index.tsx
@@ -2,7 +2,15 @@ import type { Props } from './types';
 import './InstructionCard.css';
 
 function InstructionCard({ number, title, instructionText }: Props) {
-	return <div className='InstructionCard'></div>;
+	return (
+		<div className='InstructionCard'>
+			<h2 className='InstructionCard__heading'>
+				<span className='InstructionCard__number'>{number}</span>
+				{title}
+			</h2>
+			<p className='InstructionCard__content'>{instructionText}</p>
+		</div>
+	);
 }
 
 export { InstructionCard };
