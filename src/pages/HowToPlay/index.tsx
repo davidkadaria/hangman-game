12966b3e3421b2ff233866d668b0b429c35ca38f
DavidKadaria <david.qada@gmail.com>
2024-04-12 00:25:36 +0400

    Modify HowToPlay page: add InstructionCard for each instruction

--- src/pages/HowToPlay/index.tsx
+++ src/pages/HowToPlay/index.tsx
@@ -1,11 +1,21 @@
-import { Header } from '../../components';
+import { Header, InstructionCard } from '../../components';
 import { pageHierarchy } from '../../constants';
+import { instructionsData } from './data';
 import type { Props } from '../commonTypes';
 
 function HowToPlay({ setPage }: Props) {
 	return (
 		<>
 			<Header heading='How to Play' goBack={() => setPage(pageHierarchy.home.id)} />
+			<div className='HowToPlay__instructions'>
+				{instructionsData.map((instruction) => (
+					<InstructionCard
+						number={instruction.id}
+						title={instruction.title}
+						instructionText={instruction.content}
+					/>
+				))}
+			</div>
 		</>
 	);
 }
