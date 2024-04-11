a074d9bfe21690112a0f514e689bfabf125ec73f
DavidKadaria <david.qada@gmail.com>
2024-04-11 23:17:12 +0400

    Modify Home page; Use Icon instead of static texts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,9 @@
 import type { Props } from '../commonTypes';
 import { PopupCard } from '../../components';
+import { IconLogo } from '../../icons';
 
 function Home({ setPage }: Props) {
-	return (
-		<PopupCard supTitle='The' title='Hangman' subTitle='Game'>
-			Home
-		</PopupCard>
-	);
+	return <PopupCard title={<IconLogo />}>Home</PopupCard>;
 }
 
 export { Home };
