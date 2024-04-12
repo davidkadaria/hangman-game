cae5a35b3561616b8a8e7c39440740146dc4cd2c
DavidKadaria <david.qada@gmail.com>
2024-04-12 17:55:27 +0400

    Create parseData util module to extract data from the data.json file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './queryParams';
 export * from './pages';
+export * from './parseData';
---
+++ src/utils/parseData.ts
@@ -0,0 +1,5 @@
+import data from '../data.json';
+
+const categoriesList = Object.keys(data.categories);
+
+export { categoriesList };
