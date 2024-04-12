b7a45ba65f3b6572c302b7cbcae8a0386a59fecf
DavidKadaria <david.qada@gmail.com>
2024-04-12 16:01:39 +0400

    Modify PickCategory page: add the way to pick category

--- src/pages/PickCategory/index.tsx
+++ src/pages/PickCategory/index.tsx
@@ -3,14 +3,20 @@ import { pageHierarchy } from '../../constants';
 import type { Props } from '../commonTypes';
 import './PickCategory.css';
 
-function PickCategory({ setPage, categories }: Props) {
+function PickCategory({ setPage, categories, setCategory }: Props) {
 	return (
 		<>
 			<Header heading='Pick a Category' goBack={() => setPage(pageHierarchy.home.id)} />
 			<div className='PickCategory__category-list'>
 				{categories &&
 					categories.map((category) => (
-						<CategoryItem key={category} category={category} onClick={() => {}} />
+						<CategoryItem
+							key={category}
+							category={category}
+							onClick={() => {
+								setCategory && setCategory(category);
+							}}
+						/>
 					))}
 			</div>
 		</>
