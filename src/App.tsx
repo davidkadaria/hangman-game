a6a201dead8e344eba054dcdac4e2bafebf6ff54
DavidKadaria <david.qada@gmail.com>
2024-04-12 16:07:50 +0400

    Modify the root element (App.tsx): add category-related functionality and validators

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { useState, useEffect, useCallback, useMemo } from 'react';
-import { getQueryParam, setQueryParam, isPageValid, getCategories } from './utils';
+import {
+	getQueryParam,
+	setQueryParam,
+	removeQueryParam,
+	isPageValid,
+	getCategories,
+	isValidCategory,
+} from './utils';
 import { pageHierarchy, defaultPage } from './constants';
 
 import './styles/theme.css';
@@ -8,17 +15,35 @@ import './App.css';
 
 function App() {
 	const [currentPage, setCurrentPage] = useState<string>(defaultPage);
+	const [currentCategory, setCurrentCategory] = useState<string | null>(null);
+
 	const categories = useMemo(() => getCategories(), []);
 
-	const setPage = useCallback((page: string) => {
+	const setPage = useCallback((page: string): void => {
 		setCurrentPage(page);
 		setQueryParam('page', page);
 	}, []);
 
+	const setCategory = useCallback(
+		(category?: string): void => {
+			if (!category || !isValidCategory(category, categories)) {
+				setCurrentCategory(null);
+				removeQueryParam('category');
+			} else {
+				setCurrentCategory(category);
+				setQueryParam('category', category);
+			}
+		},
+		[categories]
+	);
+
 	const handleHistoryChange = useCallback(() => {
 		const page = getQueryParam('page') || defaultPage;
+		const category = getQueryParam('category');
+
 		setPage(page);
-	}, [setPage]);
+		category && page === pageHierarchy.gamePlay.id && setCategory(category);
+	}, [setPage, setCategory]);
 
 	useEffect(() => {
 		handleHistoryChange();
@@ -35,9 +60,20 @@ function App() {
 		setPage(defaultPage);
 	}
 
+	if (currentPage === pageHierarchy.pickCategory.id && currentCategory) {
+		setPage(pageHierarchy.gamePlay.id);
+	} else if (currentPage !== pageHierarchy.gamePlay.id && currentCategory) {
+		setCategory();
+	} else if (
+		currentPage === pageHierarchy.gamePlay.id &&
+		(!currentCategory || !isValidCategory(currentCategory, categories))
+	) {
+		setPage(pageHierarchy.pickCategory.id);
+	}
+
 	const pageProps = {
 		setPage,
-		...(currentPage === pageHierarchy.pickCategory.id && { categories }),
+		...(currentPage === pageHierarchy.pickCategory.id && { categories, setCategory }),
 	};
 
 	return <div className='App'>{pageHierarchy[currentPage].component(pageProps)}</div>;
