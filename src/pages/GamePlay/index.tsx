1f2005c40f9cf63ddad328e6141de1b731eed46e
DavidKadaria <david.qada@gmail.com>
2024-04-12 16:56:23 +0400

    Moidify GamePlay page: partially implement the markup of the page

--- src/pages/GamePlay/index.tsx
+++ src/pages/GamePlay/index.tsx
@@ -1,7 +1,25 @@
+import { IconMenu, IconHeart } from '../../icons';
 import type { Props } from '../commonTypes';
 
-function GamePlay({ setPage }: Props) {
-	return <h1>Game Play</h1>;
+function GamePlay({ setPage, category }: Props) {
+	return (
+		<>
+			<header className='GamePlay__header'>
+				<div className='GamePlay__menu'>
+					<IconMenu />
+				</div>
+				<h1 className='GamePlay__category-name'>{category}</h1>
+				<div className='GamePlay__health'>
+					<div className='GamePlay__health-indicator'>
+						<span className='GamePlay__health-current'></span>
+					</div>
+					<IconHeart />
+				</div>
+			</header>
+
+			<main className='GamePlay__main'></main>
+		</>
+	);
 }
 
 export { GamePlay };
