dc598a44f65841ecfb542a8c8a9f5d10cf4bc188
DavidKadaria <david.qada@gmail.com>
2024-04-12 18:39:42 +0400

    Modify PickCategory page: add CategoryItem and iterate over categories list

--- src/pages/PickCategory/index.tsx
+++ src/pages/PickCategory/index.tsx
@@ -1,11 +1,17 @@
-import { Header } from '../../components';
+import { Header, CategoryItem } from '../../components';
 import { pageHierarchy } from '../../constants';
 import type { Props } from '../commonTypes';
 
-function PickCategory({ setPage }: Props) {
+function PickCategory({ setPage, categories }: Props) {
 	return (
 		<>
 			<Header heading='Pick a Category' goBack={() => setPage(pageHierarchy.home.id)} />
+			<div className='PickCategory__category-list'>
+				{categories &&
+					categories.map((category) => (
+						<CategoryItem key={category} category={category} onClick={() => {}} />
+					))}
+			</div>
 		</>
 	);
 }
