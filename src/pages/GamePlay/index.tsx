5651edb1033493076c4609155cfbaff5c716a2e2
DavidKadaria <david.qada@gmail.com>
2024-04-19 22:07:24 +0400

    Modify GamePlay page JSX structure & add styles

--- src/pages/GamePlay/index.tsx
+++ src/pages/GamePlay/index.tsx
@@ -1,14 +1,17 @@
 import { IconMenu, IconHeart } from '../../icons';
 import type { Props } from '../commonTypes';
+import './GamePlay.css';
 
 function GamePlay({ setPage, category }: Props) {
 	return (
 		<>
 			<header className='GamePlay__header'>
-				<div className='GamePlay__menu'>
-					<IconMenu />
+				<div className='GamePlay__heading'>
+					<div className='GamePlay__menu'>
+						<IconMenu />
+					</div>
+					<h1 className='GamePlay__category-name'>{category}</h1>
 				</div>
-				<h1 className='GamePlay__category-name'>{category}</h1>
 				<div className='GamePlay__health'>
 					<div className='GamePlay__health-indicator'>
 						<span className='GamePlay__health-current'></span>
