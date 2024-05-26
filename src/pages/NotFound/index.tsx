d70516f39161211ae3be79498aede6ab58f4e5f8
DavidKadaria <david.qada@gmail.com>
2024-05-26 23:27:44 +0400

    Create NotFound page which redirects back to the home page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function NotFound() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		navigate('/', { replace: true });
+	}, [navigate]);
+
+	return <></>;
+}
+
+export { NotFound };
