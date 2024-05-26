8d4837cf334f4a72f5e4372404a25ef1b7bb7eeb
DavidKadaria <david.qada@gmail.com>
2024-05-26 23:27:01 +0400

    Remove unneeded files

--- src/utils/pages.ts
+++
@@ -1,9 +0,0 @@
-import { pageHierarchy } from '../constants';
-
-function isPageValid(page: string): boolean {
-	const pageIds = Object.keys(pageHierarchy).map((key) => pageHierarchy[key].id);
-
-	return pageIds.includes(page);
-}
-
-export { isPageValid };
