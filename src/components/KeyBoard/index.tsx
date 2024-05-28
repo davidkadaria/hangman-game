79cbebc04ee0658c67fdc09f988b5a1436285e00
DavidKadaria <david.qada@gmail.com>
2024-05-28 18:26:52 +0400

    Initialize KeyBoard component

---
+++ src/components/KeyBoard/index.tsx
@@ -0,0 +1,8 @@
+import type { Props } from './types';
+import './KeyBoard.css';
+
+function KeyBoard({}: Props) {
+	return <div className='KeyBoard'></div>;
+}
+
+export { KeyBoard };
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from './Header';
 export * from './InstructionCard';
 export * from './CategoryItem';
 export * from './WordBoard';
+export * from './KeyBoard';
