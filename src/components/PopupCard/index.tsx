1e117b5ef0a9dcc46eacc37413aaff015ad6270f
DavidKadaria <david.qada@gmail.com>
2024-05-28 21:35:59 +0400

    Change JSX tag for heading

--- src/components/PopupCard/index.tsx
+++ src/components/PopupCard/index.tsx
@@ -5,7 +5,7 @@ function PopupCard({ title, darkenBackground, children }: Props) {
 	return (
 		<>
 			<div className='PopupCard'>
-				<h1 className='PopupCard__heading'>{title}</h1>
+				<div className='PopupCard__heading'>{title}</div>
 				<div className='PopupCard__content'>{children}</div>
 			</div>
 			{darkenBackground && <div className='PopupCard__background' />}
