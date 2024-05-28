80cbb930bdf7ae96838eeff5e9c83ba9bf941ac7
DavidKadaria <david.qada@gmail.com>
2024-05-28 18:30:07 +0400

    Rename and change keyboard module

--- src/constants/englishAlphabet.ts
+++ src/constants/englishAlphabet.ts
@@ -1,7 +1,3 @@
-const keyboard = [
-	['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
-	['j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'],
-	['s', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-];
+const englishAlphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-export { keyboard };
+export { englishAlphabet };
